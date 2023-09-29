47c0ec4dc4b7400c839c18ed7fd000b31cd26298
Pedro <pedropberger@gmail.com>
2023-09-29 17:00:46 -0300

    fix: e-mail title

--- email.R
+++ email.R
@@ -12,10 +12,13 @@ cat("processando email")
 #### Envia o e-mail com authentication and SSL
 sendmail(from="age@mpes.mp.br", 
          to=c(email), 
-         subject="Formulário do Plano de Atuação das Promotorias de Justiça", 
+         subject="Relatório NAVV", 
          msg=list(mime_part(texto), mime_part(filename_pdf)), 
          #msg=list(mime_part(texto), mime_part(filename_pdf), mime_part(filename_word)), 
          engine = "curl", 
          engineopts = list(username = username, password = password), 
          control=list(smtpServer="smtp://smtp.office365.com:587", verbose = TRUE) 
 )
+
+
+
